a85b6f9fcc1c7cd88c272e51641d5ea21dc90fdd
Hudaibkhan <161836104+Hudaibkhan@users.noreply.github.com>
2024-03-06 01:13:24 +0500

    Add files via upload
    I am student of Governor Sindh IT program

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+// Day:2 Challenge
+// QUESTION 4
+// console.log('"Elbert Einstein once said, "A person who never made a mistake never tried anything new.""');
+// QUESTION 5
+// let famous_person: string="Albert Einstein";
+// let message: string ="A person who never made a mistake never tried anything new.";
+// console.log(`"${famous_person} once said," ${message}""`);
+// QUESTION 6
+// let personName: string="\n\tHudaib\t\n";
+// console.log(personName);
+// let stripped: string = personName.trim();
+// console.log(stripped);
